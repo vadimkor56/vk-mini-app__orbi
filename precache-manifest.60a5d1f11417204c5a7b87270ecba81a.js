dfa970ba50df5b54c1d52ebb7630427ea93f5993
Vadim Koperanov <vadim-kor@yandex-team.ru>
2019-09-27 18:39:17 +0300

    Updates

---
+++ precache-manifest.60a5d1f11417204c5a7b87270ecba81a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bc2ac9f3299eb518703",
+    "url": "/vk-mini-app__orbi/static/css/main.8001dd22.chunk.css"
+  },
+  {
+    "revision": "6bc2ac9f3299eb518703",
+    "url": "/vk-mini-app__orbi/static/js/main.0bcd3422.chunk.js"
+  },
+  {
+    "revision": "81cbff8929972741906c",
+    "url": "/vk-mini-app__orbi/static/js/runtime~main.3fa7ecb7.js"
+  },
+  {
+    "revision": "9170e1a10e8802fee7fc",
+    "url": "/vk-mini-app__orbi/static/css/2.7e1ef651.chunk.css"
+  },
+  {
+    "revision": "9170e1a10e8802fee7fc",
+    "url": "/vk-mini-app__orbi/static/js/2.442f4989.chunk.js"
+  },
+  {
+    "revision": "a19996a9c8adde695f2dc949fd47f20c",
+    "url": "/vk-mini-app__orbi/index.html"
+  }
+];
